4a2b88aa18cdb00497d9cc4115736166d10f469b
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-02-23 01:36:58 +0300

    set up repositories cell

---
+++ GithubClient/GitHub/UI/Repositories/Cell/RepositoryTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoryTableViewCell.swift
+//  GithubClient
+//
+//  Created by Pavel Boltromyuk on 2/23/21.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var starCountLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var colorLanguageImage: UIImageView!
+}
--- GithubClient/GitHub/UI/Repositories/RepositoriesViewController.swift
+++ GithubClient/GitHub/UI/Repositories/RepositoriesViewController.swift
@@ -9,19 +9,31 @@ import UIKit
 
 class RepositoriesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        RepositoryTableViewCell.registerCellNib(in: tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDataSource&UITableViewDelegate
+extension RepositoriesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = RepositoryTableViewCell.dequeueReusableCell(in: tableView, for: indexPath)
+        
+        return cell
     }
-    */
-
+    
 }
