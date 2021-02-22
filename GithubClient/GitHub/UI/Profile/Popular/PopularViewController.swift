73bb30fdc11e562969561c5d954a1b0f4895203e
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-02-23 00:30:30 +0300

    add containerView and collectionView

---
+++ GithubClient/GitHub/UI/Profile/Popular/PopularViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PopularViewController.swift
+//  GithubClient
+//
+//  Created by Pavel Boltromyuk on 2/23/21.
+//
+
+import UIKit
+
+class PopularViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
