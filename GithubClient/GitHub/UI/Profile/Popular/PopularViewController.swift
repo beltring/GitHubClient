958468a2fc1e558d0350c1184b15af5a4b892b15
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-02-23 01:04:46 +0300

    set up collection view cell

--- GithubClient/GitHub/UI/Profile/Popular/PopularViewController.swift
+++ GithubClient/GitHub/UI/Profile/Popular/PopularViewController.swift
@@ -9,21 +9,48 @@ import UIKit
 
 class PopularViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupCollectionView() {
+        PopularCollectionViewCell.registerCellNib(in: collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-    */
 
+}
+
+// MARK: - UICollectionViewDataSource&UICollectionViewDelegate
+extension PopularViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = PopularCollectionViewCell.dequeueReusableCell(in: collectionView, for: indexPath)
+        cell.layer.borderColor = UIColor.Popular.cellFrame.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension PopularViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 90, height: 170)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 5)
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 20
+//    }
 }
---
+++ GithubClient/GitHubHelper/UIKit/UIColor/UIColor+GithubClient.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+GithubClient.swift
+//  GithubClient
+//
+//  Created by Pavel Boltromyuk on 2/23/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    enum Popular {
+        static var cellFrame = #colorLiteral(red: 0.6783918707, green: 0.6783918707, blue: 0.6783918707, alpha: 1)
+    }
+}
