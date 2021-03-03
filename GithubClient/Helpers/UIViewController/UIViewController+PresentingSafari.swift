835258c77de0e924feafef4aba26fad40776707c
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-03-03 19:51:57 +0300

    add extension for UIViewController

---
+++ GithubClient/Helpers/UIViewController/UIViewController+PresentingSafari.swift
@@ -0,0 +1,35 @@
+//
+//  UIViewController+PresentingSafari.swift
+//  GithubClient
+//
+//  Created by Pavel Boltromyuk on 3/3/21.
+//
+
+import Foundation
+import SafariServices
+
+extension UIViewController {
+    func presentSafariViewController(url: URL?) {
+        guard let url = url else {
+            return
+        }
+        
+        let vc = self.makeSafariViewController(url: url)
+        self.present(vc, animated: true)
+    }
+
+    func makeSafariViewController(url: URL) -> SFSafariViewController {
+        let vc = SFSafariViewController(url: url)
+        vc.delegate = UIViewController.safariDelegate
+        
+        return vc
+    }
+    
+    private static let safariDelegate = WebViewControllerDelegate()
+}
+
+fileprivate class WebViewControllerDelegate: NSObject, SFSafariViewControllerDelegate {
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
