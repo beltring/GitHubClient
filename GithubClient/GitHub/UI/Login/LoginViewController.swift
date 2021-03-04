8ce9a0868516cf947af43bdb36dee1b214655e15
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-03-04 12:03:49 +0300

    added reachability manager

--- GithubClient/GitHub/UI/Login/LoginViewController.swift
+++ GithubClient/GitHub/UI/Login/LoginViewController.swift
@@ -11,38 +11,14 @@ import OAuthSwift
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var signInButton: UIButton!
-    private let reachability = try! Reachability()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
-        do {
-            try reachability.startNotifier()
-        }
-        catch {
-            print("could not start reachability notifier")
-        }
         
         signInButton.layer.cornerRadius = 20
     }
     
     @IBAction func signInTapped(_ sender: UIButton) {
         GitHubAuthService.shared.auth(viewController: self)
-    }
-    
-    @objc func reachabilityChanged(note: Notification) {
-        
-        let reachability = note.object as! Reachability
-        
-        switch reachability.connection {
-        case .unavailable:
-            signInButton.isHidden = true
-            presentAlert(title: "Error", message: "Network not reachable", preferredStyle: .alert, cancelStyle: .destructive)
-        default:
-            if signInButton.isHidden {
-                signInButton.isHidden.toggle()
-            }
-        }
     }
 }
