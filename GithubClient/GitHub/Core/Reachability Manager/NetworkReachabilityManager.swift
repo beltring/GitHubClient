d87363278d04ee7f3bc18de88baf973804c23d23
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-04-07 15:38:38 +0300

    refactoring reachability manager

--- GithubClient/GitHub/Core/Reachability Manager/NetworkReachabilityManager.swift
+++ GithubClient/GitHub/Core/Reachability Manager/NetworkReachabilityManager.swift
@@ -11,57 +11,36 @@ import Reachability
 class NetworkReachabilityManager: NSObject {
     
     var reachability: Reachability!
-        static let sharedInstance: NetworkReachabilityManager = {
-            return NetworkReachabilityManager()
-        }()
-        override init() {
-            super.init()
-            reachability = try! Reachability()
-            NotificationCenter.default.addObserver(
-                self,
-                selector: #selector(networkStatusChanged(_:)),
-                name: .reachabilityChanged,
-                object: reachability
-            )
-            do {
-                try reachability.startNotifier()
-            } catch {
-                print("Unable to start notifier")
-            }
+    
+    static let sharedInstance: NetworkReachabilityManager = {
+        return NetworkReachabilityManager()
+    }()
+    
+    private override init() {
+        super.init()
+        reachability = try! Reachability()
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(networkStatusChanged(_:)),
+            name: .reachabilityChanged,
+            object: reachability
+        )
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
         }
-        @objc func networkStatusChanged(_ notification: Notification) {
-            print("network status changed")
+    }
+    
+    @objc func networkStatusChanged(_ notification: Notification) {
+        print("network status changed")
+    }
+    
+    static func stopNotifier() -> Void {
+        do {
+            try (NetworkReachabilityManager.sharedInstance.reachability).startNotifier()
+        } catch {
+            print("Error stopping notifier")
         }
-        static func stopNotifier() -> Void {
-            do {
-                try (NetworkReachabilityManager.sharedInstance.reachability).startNotifier()
-            } catch {
-                print("Error stopping notifier")
-            }
-        }
-
-        // Network is reachable
-        static func isReachable(completed: @escaping (NetworkReachabilityManager) -> Void) {
-            if (NetworkReachabilityManager.sharedInstance.reachability).connection != .unavailable {
-                completed(NetworkReachabilityManager.sharedInstance)
-            }
-        }
-        // Network is unreachable
-        static func isUnreachable(completed: @escaping (NetworkReachabilityManager) -> Void) {
-            if (NetworkReachabilityManager.sharedInstance.reachability).connection == .unavailable {
-                completed(NetworkReachabilityManager.sharedInstance)
-            }
-        }
-        // Network is reachable via WWAN/Cellular
-        static func isReachableViaWWAN(completed: @escaping (NetworkReachabilityManager) -> Void) {
-            if (NetworkReachabilityManager.sharedInstance.reachability).connection == .cellular {
-                completed(NetworkReachabilityManager.sharedInstance)
-            }
-        }
-        // Network is reachable via WiFi
-        static func isReachableViaWiFi(completed: @escaping (NetworkReachabilityManager) -> Void) {
-            if (NetworkReachabilityManager.sharedInstance.reachability).connection == .wifi {
-                completed(NetworkReachabilityManager.sharedInstance)
-            }
-        }
+    }
 }
