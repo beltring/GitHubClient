a5177396b9587618e0e5928710e3101499a72941
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-04-08 09:32:01 +0300

    add popTip in commits screen

--- GithubClient/GitHub/UI/Commits/CommitsViewController.swift
+++ GithubClient/GitHub/UI/Commits/CommitsViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Pavel Boltromyuk on 2/25/21.
 //
 
+import AMPopTip
 import UIKit
 
 class CommitsViewController: UIViewController {
@@ -15,13 +16,15 @@ class CommitsViewController: UIViewController {
     private let commitsService = CommitsApiService()
     private var commitsData = [CommitData]()
     private var activityIndicatorView: UIActivityIndicatorView!
+    private let popTip = PopTip()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.title = "Commits"
+        setupNavigationItem()
         setupTableView()
         setupActivityIndicator()
+        setupPopTip()
         self.activityIndicatorView.startAnimating()
         fetchCommits()
     }
@@ -38,6 +41,29 @@ class CommitsViewController: UIViewController {
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
         tableView.backgroundView = activityIndicatorView
         self.activityIndicatorView = activityIndicatorView
+    }
+    
+    private func setupNavigationItem() {
+        self.navigationItem.title = "Commits"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .done, target: self, action: #selector(tappedInformation(_:)))
+    }
+    
+    private func setupPopTip() {
+        popTip.font = UIFont(name: "Avenir-Medium", size: 12)!
+        popTip.shouldDismissOnTap = true
+        popTip.edgeMargin = 5
+        popTip.offset = 2
+        popTip.bubbleOffset = 0
+        popTip.edgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        popTip.bubbleColor = UIColor(red: 0.31, green: 0.57, blue: 0.87, alpha: 1)
+    }
+    
+    @objc func tappedInformation(_ sender: UIBarButtonItem) {
+        let coordinates = CGRect(x: tableView.frame.maxX, y: 0, width: 0, height: 0)
+        popTip.show(text: "Сommits from the default branch", direction: .autoHorizontal, maxWidth: 200, in: tableView, from: coordinates)
+        var _ = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (_) in
+            self.popTip.hide()
+        }
     }
 }
 
